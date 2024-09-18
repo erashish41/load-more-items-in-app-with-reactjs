36fead6d6ed91b777d577b0ff7aa2cc7f459d031
Ashish Bhardwaj <erashish41@gmail.com>
2024-09-18 17:34:46 +0530

    LoadMoreItem component updated

---
+++ load-more-items/src/App.jsx
@@ -0,0 +1,11 @@
+import { LoadMoreItems } from "./components/LoadMoreItems";
+
+const App = () => {
+  return (
+    <div>
+      <LoadMoreItems />
+    </div>
+  )
+}
+
+export default App;
---
+++ load-more-items/src/components/LoadMoreItems.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+
+export const LoadMoreItems = () => {
+
+    const [products, setProducts] = useState([]);
+    const [count, setCount] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [disableButton, setDisableButton] = useState(false);
+
+    async function fetchProducts() {
+        try {
+            setLoading(true);
+            const response = await fetch(
+                `https://dummyjson.com/products?limit=20&skip=${
+                    count === 0 ? 0 : count * 20
+                }`
+            );
+            const result = await response.json();
+            console.log(result);
+            
+            if(result && result.products && result.products.length){
+                setProducts((prevProducts) => [...prevProducts, ...result.products]);
+                setLoading(false);
+            }
+            
+        } catch (e) {
+            setError(e.message)
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    },[count]);
+
+    useEffect(() => {
+        if(products && products.length === 100) setDisableButton(true);
+    },[products])
+
+    if(loading){
+       return <div>Loading data! Please wait...</div>
+    }
+
+    if(error){
+        return <div>Error occured! Please check your Code again.</div>
+    }
+
+    return (
+        <div className="container">
+            <div className="product-container">
+                {
+                    products && products.length > 0 
+                    ? products.map((currElem, index) => {
+                        return <div className="product" key={currElem.id}>
+                            <img 
+                                src={currElem.thumbnail}
+                                alt={currElem.title}
+                            />
+                            <p>{currElem.title}</p>
+                        </div>
+                    }) 
+                    : null
+                }
+            </div>
+            <button 
+                onClick={() => setCount(count + 1)}
+                className="button-container"
+                disabled={disableButton}
+                >
+                Load more items
+            </button>
+            {
+                disableButton ? <div>You have reached 100 limit</div> : null
+            }
+        </div>
+    )
+}
